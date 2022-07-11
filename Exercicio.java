129670074a9682d9a2c4a19184ef8f633118781f
JoaoFontanella <107406481+JoaoFontanella@users.noreply.github.com>
2022-07-11 11:58:58 -0300

    Add files via upload

---
+++ Exercicio.java
@@ -0,0 +1,34 @@
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+public class Exercicio{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int idade;
+        System.out.println("Digite sua idade: ");
+        idade=scan.nextInt();
+        if(idade <= 3){
+            JOptionPane.showMessageDialog(null, "Bebe");
+        }
+        if((idade >= 4) && (idade <=9)){
+            JOptionPane.showMessageDialog(null, "Criança");
+    }
+    if((idade >= 10) && (idade <=13)){
+        JOptionPane.showMessageDialog(null, "Pré-Adolescente");
+    }
+    if((idade >= 14) && (idade <=17)){
+        JOptionPane.showMessageDialog(null, "Adolescente");
+    }
+    if((idade >=18 ) && (idade <=24)){
+        JOptionPane.showMessageDialog(null, "Jovem");
+    }
+    if((idade >=25 ) && (idade <=40)){
+        JOptionPane.showMessageDialog(null, "Meia-Idade");
+    }
+    if((idade >=41 ) && (idade <=60)){
+        JOptionPane.showMessageDialog(null, "Novo,Cheio de Ruga");
+}
+ }
+}
+
+
---
+++ JOption.java
@@ -0,0 +1,9 @@
+
+import javax.swing.*;
+public class JOption{
+
+    public static void main(String[] args){
+        String nome = "turma 3-53";
+        JOptionPane.showMessageDialog(null,"A " + nome + " é a Melhor? ");
+    }
+} 
